a54d7bfeabd89720ec8935fe0df60bbd5a6e3d5e
Job85 <julianjernigan@ymail.com>
2022-05-27 11:21:42 -0400

    installed dotenv

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 const express = require('express')
 const cors = require('cors')
 const logger = require('morgan')
+const db = require('./db')
+const AppRouter = require('./routes/AppRouter')
+
+const app = express()
+
 const PORT = process.env.PORT || 3001
 
 
-const app = express()
 
 app.use(cors())
 app.use(express.json())
 app.use(logger('dev'))
 
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+app.use('/api', AppRouter)
 app.get('/', (req, res) => {
     res.send('This is root!')
 })
