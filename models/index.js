bc15e50801bdc0edf7544338f3e12f7b80b7c905
Job85 <julianjernigan@ymail.com>
2022-06-03 17:37:35 -0400

    created user schema

--- models/index.js
+++ models/index.js
@@ -3,14 +3,18 @@ const ItemSchema = require('./item')
 const InventorySchema = require('./inventory')
 const VendorSchema = require('./vendor')
 const Vendor_ContactSchema = require('./vendor_contact')
+const UserSchema = require('./user')
 
 const Item = mongoose.model('Item', ItemSchema)
 const Inventory = mongoose.model('Inventory', InventorySchema)
 const Vendor = mongoose.model('Vendor', VendorSchema)
 const Vendor_Contact = mongoose.model('Vendor_Contact', Vendor_ContactSchema)
+const User = mongoose.model('User', UserSchema)
+
 module.exports = {
     Item,
     Inventory,
     Vendor,
-    Vendor_Contact
+    Vendor_Contact,
+    User
 }
