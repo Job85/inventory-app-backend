a2cf46f7199412845f1bebd8cb463ff19cd77cfe
Job85 <julianjernigan@ymail.com>
2022-06-02 20:08:50 -0400

    added vendor_code to schema

--- models/item.js
+++ models/item.js
@@ -9,7 +9,8 @@ const Item = new Schema(
         description: { type: String, require: true },
         unit_measure: { type: String, require: true },
         case_size: { type: String, require: true },
-        vendor_name: { type: String, require: true }
+        vendor_name: { type: String, require: true },
+        vendor_code: { type: String }
     },
     { timestamps: true }
 )
