d0b2b296af3017e84ceb2be0c96d9c762fc71504
Job85 <julianjernigan@ymail.com>
2022-06-03 09:31:49 -0400

    snaked_cased password_digest

--- models/user.js
+++ models/user.js
@@ -4,6 +4,6 @@ const User = new Schema(
     {
         _id: { type: Schema.Types.ObjectId, default: null },
         username: { type: String, require: true },
-        passwordDigest: { type: Text, require: true }
+        password_digest: { type: Text, require: true }
     }
 )
