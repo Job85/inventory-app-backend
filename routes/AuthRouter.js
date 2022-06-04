66cfcdd6fab043f47415f866bcc4e18215ea13b2
Job85 <julianjernigan@ymail.com>
2022-06-04 10:34:28 -0400

    created Auth Routes for: post.Register, post.Login, put.UpdatePassword

---
+++ routes/AuthRouter.js
@@ -0,0 +1,12 @@
+const Router = require('express').Router()
+const controller = require('../controllers/AuthController')
+const middleware = require('../middleware')
+
+Router.post('/register', controller.Register)
+Router.post('/login', controller.Login)
+Router.put('/update/:user_id',
+    middleware.stripToken,
+    middleware.verifyToken,
+    controller.UpdatePassword)
+
+module.exports = Router
--- routes/UserRouter.js
+++
@@ -1,9 +0,0 @@
-const Router = require('express').Router()
-const controller = require('../controllers/UserController')
-const { protect } = require('../middleware/Auth')
-
-Router.post('/', controller.registerUser)
-Router.post('/login', controller.loginUser)
-Router.get('/me', protect, controller.getMe)
-
-module.exports = Router
