a29b9c210a682cb351fb61f3f0695ac87b1e6e39
Job85 <julianjernigan@ymail.com>
2022-06-04 10:55:01 -0400

    imported user router

--- routes/AppRouter.js
+++ routes/AppRouter.js
@@ -3,11 +3,13 @@ const VendorRouter = require('./VendorRouter')
 const ItemRouter = require('./ItemRouter')
 const InventoryRouter = require('./InventoryRouter')
 const AuthRouter = require('./AuthRouter')
+const UserRouter = require('./UserRouter')
 
 Router.use('/vendor', VendorRouter)
 Router.use('/item', ItemRouter)
 Router.use('/inventory', InventoryRouter)
 Router.use('/auth', AuthRouter)
+Router.use('/user', UserRouter)
 
 
 module.exports = Router
