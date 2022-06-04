3e5b4700504707b8915c09461250b8895557f487
Job85 <julianjernigan@ymail.com>
2022-06-04 10:38:26 -0400

    updated auth routes

--- routes/AppRouter.js
+++ routes/AppRouter.js
@@ -2,12 +2,12 @@ const Router = require('express').Router()
 const VendorRouter = require('./VendorRouter')
 const ItemRouter = require('./ItemRouter')
 const InventoryRouter = require('./InventoryRouter')
-const UserRouter = require('./UserRouter')
+const AuthRouter = require('./AuthRouter')
 
 Router.use('/vendor', VendorRouter)
 Router.use('/item', ItemRouter)
 Router.use('/inventory', InventoryRouter)
-Router.use('/user', UserRouter)
+Router.use('/auth', AuthRouter)
 
 
 module.exports = Router
