d821c4746104c2e69c6ad07f47c79f8cbe4d1405
Job85 <julianjernigan@ymail.com>
2022-06-06 12:22:12 -0400

    deleted unused code

--- middleware/index.js
+++ middleware/index.js
@@ -1,46 +1,7 @@
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
-// const asyncHandler = require('express-async-handler')
-// const User = require('../models/user')
-
-// const registerUser = asyncHandler(async (req, res) => {
-//     const { username, email, password } = req.body
-
-//     if (!username || !email || !password) {
-//         res.status(400)
-//         throw new Error('Please provide all fields')
-//     }
-
-//     const userExists = await User.find({ email })
-
-//     if (userExists) {
-//         res.status(400)
-//         throw new Error('User already exists')
-//     }
-
-//     const salt = await bcrypt.genSalt(12)
-//     const hashedPassword = await bcrypt.hash(password, salt)
-
-//     const user = await User.create({
-//         username,
-//         email,
-//         password: hashedPassword
-//     })
-
-//     if (user) {
-//         res.status(201).json({
-//             _id: user.id,
-//             username: user.username,
-//             email: user.email,
-//             token: generateToken(user._id)
-//         })
-//     } else {
-//         res.status(400)
-//         throw new Error('Invalid user data')
-//     }
-// })
-
 require('dotenv').config()
+
 const SALT_ROUNDS = parseInt(process.env.SALT_ROUNDS)
 const APP_SECRET = `${process.env.APP_SECRET}`
 
