baf7dbba5b0dca705b93a86c2724817c2d8f43b3
Job85 <julianjernigan@ymail.com>
2022-06-22 13:53:41 -0400

    updated items to an array of objects

--- models/inventory.js
+++ models/inventory.js
@@ -11,10 +11,12 @@ const Inventory = new Schema(
         },
         inventory_date: { type: Date, require: true },
         department_name: { type: String, require: true },
-        items: {
-            type: [Item],
-            default: undefined
-        },
+        items: [
+            {
+                type: [Item],
+                default: undefined
+            }
+        ],
         count: { type: Number, require: true }
     },
     { timestamps: true }
