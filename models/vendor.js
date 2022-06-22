8382ab5b3d8ebbdc80b39d677376d7fd2bf988c6
Job85 <julianjernigan@ymail.com>
2022-06-22 12:16:50 -0400

    added vendor_address object fields

--- models/vendor.js
+++ models/vendor.js
@@ -10,7 +10,15 @@ const Vendor = new Schema(
         },
         vendor_name: { type: String, require: true },
         vendor_phone: { type: String, require: true },
-        vendor_address: { type: String, require: true }
+        vendor_address: [
+            {
+                street: { type: String, require: true },
+                street2: { type: String },
+                city: { type: String, require: true },
+                state: { type: String, require: true },
+                zip_code: { type: String, require: true }
+            }
+        ]
     },
     { timestamps: true }
 )
