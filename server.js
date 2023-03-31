c37b48a666330a873f6ff5c6a8be642dddbd527a
Job85 <joulescodinglab@gmail.com>
2023-03-30 23:12:46 -0400

    removed methods, origin from cors middle-ware and set res.header for app.get function

--- server.js
+++ server.js
@@ -10,23 +10,14 @@ const PORT = process.env.PORT || 3001
 
 
 
-app.use(cors({
-    origin:
-        ['http://localhost:3000',
-            'http://restaurant-inventory-77229.web.app',
-            'https://restaurant-inventory-77229.web.app'
-        ],
-    methods: 'GET,PUT,POST,DELETE',
-    allowedHeaders: 'Content-Type, Authorization, Origin, X-Requested-With, Accept',
-    credentials: true
-}))
+app.use(cors())
 app.use(express.json())
 app.use(logger('dev'))
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 app.use('/api', AppRouter)
 app.get('/', (req, res) => {
-    res.set('Access-Control-Allow-Origin', 'https://restaurant-inventory-77229.web.app')
+    res.header('Access-Control-Allow-Origin', 'https://restaurant-inventory-77229.web.app');
     res.send('This is root!')
 })
 
